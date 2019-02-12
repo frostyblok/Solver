86e47f01e8fed5aed57034fdb80f719f5aed482e
Oluwakunle Fakorede <oluwakunlefakorede@Oluwakunles-MacBook-Pro.local>
2019-02-12 23:57:53 +0100

    (chore) Set up application in rails

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,15 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def temp
+    redirect_to root_path
+  end
+
+  def answer
+    redirect_to root_path
+  end 
+end
